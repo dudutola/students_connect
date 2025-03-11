031107929f28483d7b30d71b916cc328d7971ae7
Dulcelene Machado <dudutola@gmail.com>
2025-03-11 14:57:58 +0000

    Added a migration to add more details to the user model A user can now login using github

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,26 @@ class User < ApplicationRecord
 
   def self.from_omniauth(access_token)
     data = access_token.info
-    user = User.where(email: data['email']).first
+    uid = access_token.uid
+    provider = access_token.provider
+    random_string = Devise.friendly_token[0, 6]
+    email = data['email'].presence || "user_#{random_string}@email.com"
+    location = access_token.extra.raw_info['location'].presence || ""
+
+    user = User.find_by(provider: provider, uid: uid)
 
     # Uncomment the section below if you want users to be created if they don't exist
     unless user
       user = User.create!(
-        email: data['email'],
-        password: Devise.friendly_token[0,20]
+        email: email,
+        password: Devise.friendly_token[0, 20],
+        username: data['nickname'],
+        name: data['name'],
+        avatar_url: data['image'],
+        location: location,
+        bio: data['bio'],
+        provider: provider,
+        uid: uid
       )
     end
     user
