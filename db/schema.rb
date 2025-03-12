cbaf8e35d7877bbdd0fc5dab561a2e5a3039ffe5
Dulcelene Machado <dudutola@gmail.com>
2025-03-12 14:20:50 +0000

    Generated model lecture and its migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_12_124529) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_12_141212) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,16 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_12_124529) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "lectures", force: :cascade do |t|
+    t.string "title"
+    t.string "group_name"
+    t.string "resource_url"
+    t.bigint "chapter_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chapter_id"], name: "index_lectures_on_chapter_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +50,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_12_124529) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "lectures", "chapters"
 end
