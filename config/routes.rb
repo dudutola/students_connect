275422683c833fa4650ac8b1de995d7e7d2818e9
Dulcelene Machado <dudutola@gmail.com>
2025-03-12 14:42:54 +0000

    Generated controller lecture and the routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :chapters, only: [:index]
-  
+  resources :chapters, only: [ :index ] do
+    resources :lectures, only: [ :index ]
+  end
+
   # Defines the root path route ("/")
   # root "posts#index"
   authenticated :user do
