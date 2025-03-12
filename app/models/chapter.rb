8f28e5e578bea25e14118e286a01c35c591cc09f
Tomas Vasco <tomasgrayvasco@gmail.com>
2025-03-12 12:52:35 +0000

    model created

---
+++ app/models/chapter.rb
@@ -0,0 +1,5 @@
+class Chapter < ApplicationRecord
+  has_many :lectures, dependent: :destroy
+
+  validates :name, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_11_143013) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_12_124529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chapters", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
