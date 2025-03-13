44963dc7af3d180bf845f4c74749f18162f8f0b3
Dulcelene Machado <dudutola@gmail.com>
2025-03-13 14:51:31 +0000

    Generated the model meeting and its migration Added associations

---
+++ app/models/meeting.rb
@@ -0,0 +1,5 @@
+class Meeting < ApplicationRecord
+  belongs_to :requester, class_name: "User"
+  belongs_to :receiver, class_name: "User"
+  belongs_to :lecture
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_12_211053) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_13_144302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,20 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_12_211053) do
     t.index ["chapter_id"], name: "index_lectures_on_chapter_id"
   end
 
+  create_table "meetings", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.string "status"
+    t.bigint "requester_id", null: false
+    t.bigint "receiver_id", null: false
+    t.bigint "lecture_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lecture_id"], name: "index_meetings_on_lecture_id"
+    t.index ["receiver_id"], name: "index_meetings_on_receiver_id"
+    t.index ["requester_id"], name: "index_meetings_on_requester_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -62,4 +76,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_12_211053) do
   add_foreign_key "lecture_users", "lectures"
   add_foreign_key "lecture_users", "users"
   add_foreign_key "lectures", "chapters"
+  add_foreign_key "meetings", "lectures"
+  add_foreign_key "meetings", "users", column: "receiver_id"
+  add_foreign_key "meetings", "users", column: "requester_id"
 end
