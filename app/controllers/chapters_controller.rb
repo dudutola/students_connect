a47fd3b155543a1a064861b97bd44fb6ef165443
Dulcelene Machado <dudutola@gmail.com>
2025-03-16 12:51:28 +0000

    Added logic to go to previous or next chapter

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -6,24 +6,11 @@ class ChaptersController < ApplicationController
   def show
     @chapter = Chapter.find(params[:id])
     authorize @chapter
-    @lectures = policy_scope(Lecture)
-
-    # id qui soit plus petit que l id actuel
-
-    # previous_chapter -> 30
-    # -> 88
-    # current_chapter -> 89
-
-    # 88, 89, 90
-    # [88,90, 123]
-    # previous_chapter = Chapter.find_by(id: @chapter.id - 1)
+    @lectures = policy_scope(Lecture.where(chapter: @chapter))
 
     all_chapters = Chapter.all
     current_chapter_index = all_chapters.find_index(@chapter)
     @previous_chapter = all_chapters[current_chapter_index - 1]
-    # @next_chapter = all_chapters[current_chapter_index + 1]
-    # raise
-    # @previous_chapter = Chapter.find_by("id < ?", @chapter.id)
-    # @next_chapter = Chapter.find_by("id > ?", @chapter.id)
+    @next_chapter = all_chapters[current_chapter_index + 1]
   end
 end
