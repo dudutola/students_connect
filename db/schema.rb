8db33cafb57373d124413956ab192f06c9e1028c
Dulcelene Machado <dudutola@gmail.com>
2025-03-17 11:00:21 +0000

    Generated availabilty model and its migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_16_100243) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_17_105914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "availabilities", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_availabilities_on_user_id"
+  end
 
   create_table "chapters", force: :cascade do |t|
     t.string "name"
@@ -79,6 +88,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_16_100243) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "availabilities", "users"
   add_foreign_key "lecture_users", "lectures"
   add_foreign_key "lecture_users", "users"
   add_foreign_key "lectures", "chapters"
