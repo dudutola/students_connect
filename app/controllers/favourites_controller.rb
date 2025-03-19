1556a950e6546e4c190fb7cf37301febf093de24
Dulcelene Machado <dudutola@gmail.com>
2025-03-19 21:07:56 +0000

    Removed turbo stream Added validation to favourites Updated logic to create and destroy favourite

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -5,26 +5,31 @@ class FavouritesController < ApplicationController
   end
 
   def toggle
-    @user = User.find(params[:user_id])
+    @user = current_user
+    @lecture_user = User.find(params[:user_id])
     authorize @user
 
-    if current_user.favourited_users.include?(@user)
-      current_user.favourited_users.delete(@user)
-      @removed = true
+    # debugger
+    if current_user.favourites.find_by(favourited_user: @lecture_user)
+
+      Favourite.find_by(favourited_user: @lecture_user).destroy
+      # current_user.favourited_users.delete(@user)
+      # @removed = true
     else
-      current_user.favourited_users << @user
-      @removed = false
+      Favourite.create!(user: @user, favourited_user: @lecture_user)
+      # current_user.favourited_users << @user
+      # @removed = false
     end
 
-    respond_to do |format|
-      format.turbo_stream do
-        if params[:from] == "index"
-          render turbo_stream: turbo_stream.replace("favourited-users", partial: "favourites/favourited_users", locals: { favourited_users: current_user.favourited_users })
-        else
-          render turbo_stream: turbo_stream.replace("favorite-icon-#{@user.id}", partial: "users/favorite_button", locals: { user: @user })
-        end
-      end
-      format.html { redirect_to user_favourites_path(current_user) }
-    end
+    # respond_to do |format|
+    #   format.turbo_stream do
+    #     if params[:from] == "index"
+    #       render turbo_stream: turbo_stream.replace("favourited-users", partial: "favourites/favourited_users", locals: { favourited_users: current_user.favourited_users })
+    #     else
+    #       render turbo_stream: turbo_stream.replace("favorite-icon-#{@user.id}", partial: "users/favorite_button", locals: { user: @user })
+    #     end
+    #   end
+    #   format.html { redirect_to user_favourites_path(current_user) }
+    # end
   end
 end
--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,4 +1,6 @@
 class Favourite < ApplicationRecord
   belongs_to :user
   belongs_to :favourited_user, class_name: "User"
+
+  validates :favourited_user, presence: true, uniqueness:  { scope: :user }
 end
