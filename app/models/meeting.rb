4ffc27f9c842ae5c45005535952a1b738cbc9e3c
Dulcelene Machado <dudutola@gmail.com>
2025-03-20 11:57:58 +0000

    Added notification messages for meeting requests, acceptance, and decline Sorted notifications to display the most recent first Linked accepted meeting notifications to their details page Improved dropdown UI for better readability

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -5,6 +5,7 @@ class Meeting < ApplicationRecord
   has_many :notifications, dependent: :destroy
 
   before_validation :set_default_status, on: :create
+  after_update :create_status_change_notification, if: :status_changed?
 
   validates :lecture, presence: true
   validates :requester, presence: true
@@ -25,5 +26,19 @@ class Meeting < ApplicationRecord
     if end_time <= start_time
       errors.add(:end_time, "must be after the start time")
     end
+  end
+
+  def create_status_change_notification
+    message = case status
+              when 'accepted'
+                "#{requester.name} accepted your meeting request at #{start_time.strftime('%I:%M %p')}"
+              when 'declined'
+                "#{requester.name} declined your meeting request at #{start_time.strftime('%I:%M %p')}"
+              else
+                "#{requester.name} requested a meeting at #{start_time.strftime('%I:%M %p')}"
+              end
+
+    Notification.create(user: receiver, message: message, meeting: self)
+    Notification.create(user: requester, message: message, meeting: self)
   end
 end
