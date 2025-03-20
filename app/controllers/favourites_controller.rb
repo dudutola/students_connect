e175b9d30e0a4b840187c918489099f01ad57eb3
Dulcelene Machado <dudutola@gmail.com>
2025-03-20 10:45:57 +0000

    WIP Added turbo stream

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -5,31 +5,31 @@ class FavouritesController < ApplicationController
   end
 
   def toggle
-    @user = current_user
-    @lecture_user = User.find(params[:user_id])
+    # @user = current_user
+    # @lecture_user = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
     authorize @user
 
-    # debugger
-    if current_user.favourites.find_by(favourited_user: @lecture_user)
-
-      Favourite.find_by(favourited_user: @lecture_user).destroy
-      # current_user.favourited_users.delete(@user)
-      # @removed = true
+    if current_user.favourited_users.include?(@user)
+      # Favourite.find_by(favourited_user: @lecture_user).destroy
+      current_user.favourited_users.delete(@user)
+      @removed = true
     else
-      Favourite.create!(user: @user, favourited_user: @lecture_user)
-      # current_user.favourited_users << @user
-      # @removed = false
+      # Favourite.create!(user: @user, favourited_user: @lecture_user)
+      # raise
+      current_user.favourited_users_ids << @user.id
+      @removed = false
     end
 
-    # respond_to do |format|
-    #   format.turbo_stream do
-    #     if params[:from] == "index"
-    #       render turbo_stream: turbo_stream.replace("favourited-users", partial: "favourites/favourited_users", locals: { favourited_users: current_user.favourited_users })
-    #     else
-    #       render turbo_stream: turbo_stream.replace("favorite-icon-#{@user.id}", partial: "users/favorite_button", locals: { user: @user })
-    #     end
-    #   end
-    #   format.html { redirect_to user_favourites_path(current_user) }
-    # end
+    respond_to do |format|
+      format.turbo_stream do
+        if params[:from] == "index"
+          render turbo_stream: turbo_stream.replace("favourited-users", partial: "favourites/favourited_users", locals: { favourited_users: current_user.favourited_users })
+        else
+          render turbo_stream: turbo_stream.replace("favorite-icon-#{@user.id}", partial: "users/favorite_button", locals: { user: @user })
+        end
+      end
+      format.html { redirect_to user_favourites_path(current_user) }
+    end
   end
 end
