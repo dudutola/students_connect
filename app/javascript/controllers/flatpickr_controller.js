6c239f6f9d3c31df0b69d0898275e7873bf7002d
Dulcelene Machado <dudutola@gmail.com>
2025-03-21 10:22:45 +0000

    Added flatpickr

---
+++ app/javascript/controllers/flatpickr_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus";
+import flatpickr from "flatpickr";
+
+// Connects to data-controller="flatpickr"
+export default class extends Controller {
+  // Define the targets in the controller
+  static targets = ["startTime", "endTime"];
+
+  connect() {
+    // Initialize Flatpickr on the start time input field
+    flatpickr(this.startTimeTarget, {
+      enableTime: true, // Enables time selection
+      dateFormat: "Y-m-d H:i", // Format: Year-Month-Day Hour:Minute
+    });
+
+    // Initialize Flatpickr on the end time input field
+    flatpickr(this.endTimeTarget, {
+      enableTime: true, // Enables time selection
+      dateFormat: "Y-m-d H:i", // Format: Year-Month-Day Hour:Minute
+    });
+  }
+}
