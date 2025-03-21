b5316d44c5587fcb3fc21ac437c72322fa3ca7c3
Dulcelene Machado <136588959+dudutola@users.noreply.github.com>
2025-03-21 11:11:40 +0000

    Update request meeting btn (#90)
    * Updated favourite association in user
    
    * Installed gem solid_cable
    
    * Changed the btn after requesting a meeting

---
+++ app/javascript/controllers/meeting_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="meeting"
+export default class extends Controller {
+  static values = { userId: Number }
+
+  sendRequest(event) {
+    event.preventDefault();
+
+    const button = event.target;
+    button.textContent = "Sending...";
+    button.disabled = true;
+
+    setTimeout(() => {
+      button.textContent = "Request Sent!";
+      button.classList.add("btn-disabled");
+    }, 5000);
+
+    fetch(`/meetings`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": document.querySelector("meta[name='csrf-token']").content
+      },
+      body: JSON.stringify({ receiver_id: this.userIdValue })
+    });
+  }
+}
